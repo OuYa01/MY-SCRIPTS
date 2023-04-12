faf303db09ef6266271a836e90363a8527eaf17f
oussama laamri <laamri2002@gmail.com>
2023-04-12 18:31:07 +0000

    scritp push anything to github

---
+++ push-to-github.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+printf "%*s\n" $(( ($(tput cols) + 21) / 2 ))  '' | tr ' ' -
+echo -e "\033[44mHello sir OuYa\033[0m"
+
+if [ -z "$1" ]
+then
+  echo "Usage: $0 <commit message>"
+  exit 1
+fi
+
+printf "%*s\n" $(( ($(tput cols) + 21) / 2 ))  '' | tr ' ' -
+echo -e "\033[44m$1\033[0m"
+git add .
+git commit -m "$1"
+git push 
+make
+
